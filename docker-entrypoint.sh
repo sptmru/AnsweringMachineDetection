bf0671a336dce38ae36332e8dd67a108c4ce8b18
Soslan Aldatov <soslanaldatov@gmail.com>
2024-08-02 17:49:36 +0400

    fix Docker image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,7 @@
 sed -i "s|PORT=8888|PORT=${PORT:-8888}|" .env
 sed -i "s|LOG_LEVEL=INFO|LOG_LEVEL=${LOG_LEVEL:-INFO}|" .env
 
+pip uninstall -y numpy
+pip install numpy==1.15.4
+
 python websocket-jambonz-server.py
